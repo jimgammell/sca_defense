1dc5ab17e182f802fb13de31e1cbbaee9131f90d
Jimmy Ian Gammell <jgammell@ee350lnx13.ecn.purdue.edu>
2022-08-24 13:09:52 -0400

    Same as below

--- datasets.py
+++ datasets.py
@@ -1,3 +1,7 @@
+import os
+import requests
+import zipfile
+import shutil
 import numpy as np
 import torch
 from torch.utils.data import Dataset
@@ -29,3 +33,42 @@ class BinaryDataset(Dataset):
     
     def __len__(self):
         return 2
+
+class GooglePowerTraceDataset(Dataset):
+    def __init__(self,
+                 attack_point,
+                 num_keys,
+                 trace_length,
+                 byte,
+                 plaintext_encoding,
+                 train=True,
+                 data_path=None,
+                 download_url=r'https://storage.googleapis.com/scaaml-public/scaaml_intro/datasets.zip'):
+        if data_path == None:
+            d = os.path.join('.', 'datasets')
+        else:
+            d = data_path
+        if not os.path.isdir(d):
+            os.mkdir(d)
+        d = os.path.join(d, 'google_scaaml')
+        if not os.path.isdir(d):
+            try:
+                os.mkdir(d)
+                d = os.path.join(d, 'train' if train else 'test')
+                os.mkdir(d)
+                temp_dir = os.path.join('.', 'temp')
+                os.mkdir(temp_dir)
+                compressed_filename = 'google_scaaml_dataset.zip'
+                r = requests.get(download_url, allow_redirects=True, timeout=10)
+                with open(os.path.join(temp_dir, compressed_filename), 'wb') as F:
+                    F.write(r.content)
+                extracted_dir = os.path.join(temp_dir, 'extracted')
+                os.mkdir(extracted_dir)
+                with zipfile.ZipFile(os.path.join(temp_dir, compressed_filename), 'r') as zip_ref:
+                    zip_ref.extractall(extracted_dir)
+            except:
+                shutil.rmtree(os.path.join('.', 'datasets', 'google_scaaml'))
+                assert False
+            finally:
+                pass#shutil.rmtree(os.path.join('.', 'temp'))
+        
