f9b61f85c585b8f09fc66a353fddcf9b63d7c7af
Jimmy Ian Gammell <jgammell@ee350lnx13.ecn.purdue.edu>
2022-08-24 17:08:21 -0400

    Code to download and install Google SCAAML dataset

--- datasets.py
+++ datasets.py
@@ -2,6 +2,7 @@ import os
 import requests
 import zipfile
 import shutil
+import random
 import numpy as np
 import torch
 from torch.utils.data import Dataset
@@ -37,13 +38,14 @@ class BinaryDataset(Dataset):
 class GooglePowerTraceDataset(Dataset):
     def __init__(self,
                  attack_point,
-                 num_keys,
                  trace_length,
                  byte,
                  plaintext_encoding,
+                 num_keys=None,
                  train=True,
                  data_path=None,
                  download_url=r'https://storage.googleapis.com/scaaml-public/scaaml_intro/datasets.zip'):
+        super().__init__()
         if data_path == None:
             d = os.path.join('.', 'datasets')
         else:
@@ -52,23 +54,57 @@ class GooglePowerTraceDataset(Dataset):
             os.mkdir(d)
         d = os.path.join(d, 'google_scaaml')
         if not os.path.isdir(d):
+            print('Downloading Google SCAAML dataset...')
             try:
-                os.mkdir(d)
-                d = os.path.join(d, 'train' if train else 'test')
+                print('Creating directory structure...')
                 os.mkdir(d)
                 temp_dir = os.path.join('.', 'temp')
                 os.mkdir(temp_dir)
                 compressed_filename = 'google_scaaml_dataset.zip'
+                print('Downloading zipped dataset...')
                 r = requests.get(download_url, allow_redirects=True, timeout=10)
                 with open(os.path.join(temp_dir, compressed_filename), 'wb') as F:
                     F.write(r.content)
+                print('Extracting dataset...')
                 extracted_dir = os.path.join(temp_dir, 'extracted')
                 os.mkdir(extracted_dir)
                 with zipfile.ZipFile(os.path.join(temp_dir, compressed_filename), 'r') as zip_ref:
                     zip_ref.extractall(extracted_dir)
+                print('Installing dataset...')
+                shutil.move(os.path.join(extracted_dir, 'datasets', 'tinyaes', 'train'),
+                            os.path.join(d, 'train'))
+                shutil.move(os.path.join(extracted_dir, 'datasets', 'tinyaes', 'test'),
+                            os.path.join(d, 'test'))
             except:
                 shutil.rmtree(os.path.join('.', 'datasets', 'google_scaaml'))
                 assert False
             finally:
-                pass#shutil.rmtree(os.path.join('.', 'temp'))
+                shutil.rmtree(os.path.join('.', 'temp'))
+        self.attack_point = attack_point
+        self.num_keys = num_keys
+        self.trace_length = trace_length
+        self.byte = byte
+        base_path = os.path.join(d, 'train' if train else 'test')
+        self.get_shard = lambda filename: np.load(os.path.join(base_path, filename))
+        self.files = [f for f in os.listdir(base_path) if f.split('.')[-1] == 'npz']
+        if num_keys != None:
+            self.files = random.choices(self.files, k=num_keys)
+        self.num_examples = 0
+        for file in self.files:
+            shard = self.get_shard(file)
+            self.num_examples += len(shard['traces'])
+        self.shard_size = len(shard['traces'])
+        
+    def __getitem__(self, idx):
+        shard_idx = idx // self.shard_size
+        trace_idx = idx % self.shard_size
+        shard = self.get_shard(self.files[shard_idx])
+        trace = shard['traces'][trace_idx]
+        plaintext = shard['pts'][self.byte]
+        ap = shard[self.attack_point][self.byte]
+        return trace, plaintext, ap
+    
+    def __len__(self):
+        return self.num_examples
+        
         
