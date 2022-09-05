3cc497997d40962110d548160a8fe265f442eabb
Jimmy Ian Gammell <jgammell@purdue.edu>
2022-09-05 12:45:50 -0400

    Added code for AntiGAN, which is yet to be tested.

--- models/multilayer_perceptron.py
+++ models/multilayer_perceptron.py
@@ -221,3 +221,105 @@ class StandardGanDiscriminator(MultilayerPerceptron):
             '\n\tParameter count: {}'.format(get_param_count(self)) +\
             '\nModel summary:\n{}'.format(self.model)
         return s
+
+class StandardAntiGanGenerator(MultilayerPerceptron):
+    def __init__(self,
+                 latent_dims,
+                 label_dims,
+                 image_shape,
+                 output_transform=nn.Tanh,
+                 hidden_layers=[128, 256, 512, 1024],
+                 hidden_activation=lambda: nn.LeakyReLU(0.2),
+                 batch_norm=[False, False, True, True, True],
+                 batch_norm_kwargs={'momentum': 0.2}):
+        
+        self.latent_dims = latent_dims
+        self.image_shape = image_shape
+        self.label_dims = label_dims
+        if self.label_dims > 0:
+            self.use_labels = True
+            n_inputs = latent_dims+label_dims
+        else:
+            self.use_labels = False
+            n_inputs = latent_dims
+            
+        super().__init__(n_inputs=n_inputs,
+                         n_outputs=int(np.prod(image_shape[1:])),
+                         hidden_layers=hidden_layers,
+                         hidden_activation=hidden_activation,
+                         batch_norm=batch_norm,
+                         batch_norm_kwargs=batch_norm_kwargs)
+        
+        if self.use_labels:
+            self.label_embedding = nn.Embedding(label_dims, label_dims)
+        self.input_transform = nn.Flatten(1, -1)
+        if type(output_transform) == str:
+            output_transform = getattr(nn, output_transform)
+        self.output_transform = output_transform()
+    
+    def forward(self, *args):
+        if self.use_labels:
+            (latent_vars, labels) = args
+            transformed_latent_vars = self.input_transform(latent_vars)
+            embedded_labels = self.label_embedding(labels).view(-1, self.label_dims)
+            logits = super().forward(torch.cat((transformed_latent_vars, embedded_labels), dim=1))
+        else:
+            (latent_vars,) = args
+            transformed_latent_vars = self.input_transform(latent_vars)
+            logits = super().forward(transformed_latent_vars)
+        output = self.output_transform(logits).view(-1, *self.image_shape[1:])
+        return output
+    
+    def __repr__(self):
+        s = 'Standard AntiGAN Generator model.' +\
+            '\n\tLatent dimensions: {}'.format(self.latent_dims) +\
+            '\n\tInput label dimensions: {}'.format(self.label_dims) +\
+            '\n\tGenerated image shape: {}'.format(self.image_shape) +\
+            '\n\tOutput activation: {}'.format(self.output_transform) +\
+            '\n\tLayer sizes: {}'.format(self.layer_sizes) +\
+            '\n\tHidden activations: {}'.format(self.hidden_activation) +\
+            '\n\tBatch norm: {}'.format(self.batch_norm) +\
+            '\n\tBatch norm kwargs: {}'.format(self.batch_norm_kwargs) +\
+            '\n\tDropout: {}'.format(self.dropout) +\
+            '\n\tParameter count: {}'.format(get_param_count(self)) +\
+            '\nModel summary:\n{}'.format(self.model)
+        return s
+                 
+
+class StandardAntiGanDiscriminator(MultilayerPerceptron):
+    def __init__(self,
+                 image_shape,
+                 n_outputs=10,
+                 output_transform=nn.Softmax,
+                 hidden_layers=[512, 256, 1],
+                 hidden_activation=lambda: nn.LeakyReLU(0.2)):
+        
+        self.image_shape = image_shape
+        n_inputs = np.prod(image_shape[1:])
+        super().__init__(n_inputs=n_inputs,
+                         n_outputs=n_outputs,
+                         hidden_layers=hidden_layers,
+                         hidden_activation=hidden_activation)
+        self.input_transform = nn.Flatten(1, -1)
+        if type(output_transform) == str:
+            output_transform = getattr(nn, output_transform)
+        self.output_transform = output_transform()
+        
+    def forward(self, x):
+        transformed_x = self.input_transform(x)
+        logits = super().forward(transformed_x)
+        output = self.output_transform(logits)
+        return output
+    
+    def __repr__(self):
+        s = 'Standard GAN Discriminator model.' +\
+            '\n\tInput image shape: {}'.format(self.image_shape) +\
+            '\n\tOutput activation: {}'.format(self.output_transform) +\
+            '\n\tLayer sizes: {}'.format(self.layer_sizes) +\
+            '\n\tHidden activations: {}'.format(self.hidden_activation) +\
+            '\n\tBatch norm: {}'.format(self.batch_norm) +\
+            '\n\tBatch norm kwargs: {}'.format(self.batch_norm_kwargs) +\
+            '\n\tDropout: {}'.format(self.dropout) +\
+            '\n\tParameter count: {}'.format(get_param_count(self)) +\
+            '\nModel summary:\n{}'.format(self.model)
+        return s
--- trials/antigan_common.py
+++ trials/antigan_common.py
@@ -1,4 +1,7 @@
-
+from tqdm import tqdm
+import numpy as np
+import torch
+from torch import nn
 
 from utils import get_print_to_log, get_filename
 print = get_print_to_log(get_filename(__file__))
@@ -13,17 +16,67 @@ class AntiGanExperiment:
                  disc_opt,
                  gen_opt,
                  device,
+                 num_classes=256,
                  latent_vars_distr=None,
-                 conditions=[],
                  use_labels=False,
-                 objective_formulation='Complement'):
-        pass
+                 objective_formulation='Complement',
+                 gen_weight_clamp=None,
+                 disc_weight_clamp=None):
+        self.eval_batch = None
+        self.disc = disc.to(device)
+        self.gen = gen.to(device)
+        self.disc_loss_fn = disc_loss_fn
+        self.gen_loss_fn = gen_loss_fn
+        self.disc_opt = disc_opt
+        self.gen_opt = gen_opt
+        self.latent_vars_distr = latent_vars_distr
+        if self.latent_vars_distr != None:
+            self.eval_latent_variables = self.latent_vars_distr.sample()
+            self.use_latent_variables = True
+        self.use_labels = use_labels
+        self.objective_formulation = objective_formulation
+        self.gen_weight_clamp = gen_weight_clamp
+        self.disc_weight_clamp = disc_weight_clamp
+    
+    def get_one_hot_labels(self, labels):
+        oh_labels = nn.functional.one_hot(labels).to(torch.float).to(self.device)
+        return oh_labels
+    
+    def get_complement_labels(self, labels):
+        oh_labels = self.get_one_hot_labels(labels)
+        comp_labels = 1. - oh_labels
+        return comp_labels
+    
+    def get_gen_loss(self, disc_logits, labels):
+        if self.objective_formulation in ['Naive']:
+            gen_loss = -self.gen_loss_fn(disc_logits, self.get_one_hot_labels(labels))
+        elif self.objective_formulation in ['Complement']:
+            gen_loss = self.gen_loss_fn(disc_logits, self.get_complement_labels(labels))
+        elif self.objective_formulation in ['Wasserstein']:
+            mean_incorrect_logits = torch.mean(disc_logits*self.get_complement_labels(labels))
+            gen_loss = -mean_incorrect_logits
+        elif self.objective_formulation in ['SimReduction']:
+            gen_loss = self.gen_loss_fn(disc_logits, torch.mean(disc_logits)*torch.ones_like(disc_logits))
+        else:
+            assert False
+        return gen_loss
+    
+    def get_disc_loss(self, disc_logits, labels):
+        if self.objective_formulation in ['Naive', 'Complement', 'SimReduction']:
+            disc_loss = self.disc_loss_fn(disc_logits, self.get_one_hot_labels(labels))
+        elif self.objective_formulation in ['Wasserstein']:
+            mean_correct_logits = torch.mean(disc_logits*self.get_one_hot_labels(labels))
+            mean_incorrect_logits = torch.mean(disc_logits*self.get_complement_labels(labels))
+            disc_loss = mean_incorrect_logits - mean_correct_logits
+        else:
+            assert False
+        return disc_loss
     
     def train_step(self, batch, train_disc=True, train_gen=True):
         self.disc.train()
         self.gen.train()
         
-        raw_images, labels = batch
+        raw_images, raw_labels = batch
         raw_images = raw_images.to(self.device)
         raw_labels = raw_labels.to(self.device)
         
@@ -34,25 +87,166 @@ class AntiGanExperiment:
                 gen_args.append(latent_variables)
             if self.use_labels:
                 gen_args.append(raw_labels)
-            if self.use_conditions:
-                conditions = self.get_conditions(raw_images.size(0))
             protective_noise = self.gen(*gen_args)
             protected_images = raw_images + protective_noise
             disc_logits = self.disc(protected_images)
+            return disc_logits
         
         if train_gen:
             disc_logits = get_disc_logits()
-            gen_loss = self.get_gen_loss(disc_logits, labels)
+            gen_loss = self.get_gen_loss(disc_logits, raw_labels)
             self.gen_opt.zero_grad()
             gen_loss.backward()
             self.gen_opt.step()
-            clamp_model_params(self.gen)
+            if self.gen_weight_clamp != None:
+                clamp_model_params(self.gen, self.gen_weight_clamp)
             
         if train_disc:
             disc_logits = get_disc_logits()
-            disc_loss = self.get_disc_loss(disc_logits, labels)
+            disc_loss = self.get_disc_loss(disc_logits, raw_labels)
             self.disc_opt.zero_grad()
             disc_loss.backward()
             self.disc_opt.step()
-            clamp_model_params(self.disc)
+            if self.disc_weight_clamp != None:
+                clamp_model_params(self.disc, self.disc_weight_clamp)
         
+    @torch.no_grad()
+    def eval_step(self, batch):
+        self.disc.eval()
+        self.gen.eval()
+        
+        raw_images, raw_labels = batch
+        raw_images = raw_images.to(self.device)
+        raw_labels = raw_labels.to(self.device)
+        
+        gen_args = []
+        if self.use_latent_variables:
+            latent_variables = self.latent_vars_distr.sample().to(self.device)
+            gen_args.append(latent_variables)
+        if self.use_labels:
+            gen_args.append(raw_labels)
+        protective_noise = self.gen(*gen_args)
+        protected_images = raw_images + protective_noise
+        disc_logits = self.disc(protected_images)
+        gen_loss = self.get_gen_loss(disc_logits, raw_labels)
+        disc_loss = self.get_disc_loss(disc_logits, raw_labels)
+        disc_preds = torch.argmax(disc_logits, dim=-1)
+        disc_acc = torch.mean(torch.eq(disc_preds, raw_labels).to(torch.float))
+        labels = raw_labels.cpu().numpy()
+        disc_confidences = nn.functional.softmax(disc_logits, dim=-1).cpu().numpy()
+        confusion_matrix = np.zeros((self.num_classes, self.num_classes))
+        for label, conf_vec in zip(labels, disc_confidences):
+            confusion_matrix[label] += conf_vec/np.count_nonzero(labels==label)
+        confusion_matrix *= (self.num_classes/raw_images.size(0))
+        return {'gen_loss': extract_rv_from_tensor(gen_loss),
+                'disc_loss': extract_rv_from_tensor(disc_loss),
+                'disc_acc': extract_rv_from_tensor(disc_acc),
+                'disc_conf_mtx': confusion_matrix}
+    
+    @torch.no_grad()
+    def sample_generated_images(self, batch):
+        self.gen.eval()
+        
+        raw_images, raw_labels = batch
+        raw_images = raw_images.to(self.device)
+        raw_labels = raw_labels.to(self.device)
+        
+        gen_args = []
+        if self.use_latent_variables:
+            latent_variables = self.eval_latent_vars.to(self.device)
+            gen_args.append(latent_variables)
+        if self.use_labels:
+            gen_args.append(raw_labels)
+        protective_noise = self.gen(*gen_args)
+        protected_images = raw_images + protective_noise
+        
+        return {'protected_images': extract_rv_from_tensor(protected_images),
+                'labels': extract_rv_from_tensor(raw_labels)}
+    
+    def train_epoch(self,
+                    dataloader,
+                    train_gen=True,
+                    train_disc=True,
+                    disc_steps_per_gen_step=None,
+                    gen_steps_per_disc_step=None):
+        if disc_steps_per_gen_step != None:
+            assert gen_steps_per_disc_step == None
+            assert train_gen == True
+            assert train_disc == True
+            disc_step = 0
+            for batch in dataloader:
+                if disc_step < disc_steps_per_gen_step-1:
+                    self.train_step(batch, train_gen=False, train_disc=True)
+                    disc_step += 1
+                else:
+                    self.train_step(batch, train_gen=True, train_disc=True)
+                    disc_step = 0
+        elif gen_steps_per_disc_step != None:
+            assert gen_steps_per_disc_step == None
+            assert train_gen == True
+            assert train_disc == True
+            gen_step = 0
+            for batch in dataloader:
+                if gen_step < gen_steps_per_disc_step-1:
+                    self.train_step(batch, train_gen=True, train_disc=False)
+                    gen_step += 1
+                else:
+                    self.train_step(batch, train_gen=True, train_disc=True)
+                    gen_step = 0
+        else:
+            for batch in dataloader:
+                self.train_step(batch, train_gen=train_gen, train_disc=train_disc)
+    
+    def eval_epoch(self,
+                   train_dataloader=None,
+                   test_dataloader=None,
+                   eval_disc_hist=True,
+                   eval_gen_hist=True,
+                   sample_gen_images=True,
+                   average_metrics=True):
+        Results = {}
+        progress_bar = tqdm(total=(len(train_dataloader) if train_dataloader != None else 0) +\
+                                  (len(test_dataloader) if test_dataloader != None else 0) +\
+                                  (1 if eval_disc_hist else 0) +\
+                                  (1 if eval_disc_hist else 0) +\
+                                  (1 if sample_gen_images else 0))
+        if train_dataloader != None:
+            Results['training_metrics'] = {}
+            for batch in train_dataloader:
+                results = self.eval_step(batch)
+                for key in results.keys():
+                    if not key in Results['training_metrics'].keys():
+                        Results['training_metrics'][key] = []
+                    Results['training_metrics'][key].append(results[key])
+                progress_bar.update(1)
+            if average_metrics:
+                for key, item in Results['training_metrics'].items():
+                    Results['training_metrics'][key] = np.mean(item, axis=0)
+        if test_dataloader != None:
+            Results['test_metrics'] = {}
+            for batch in test_dataloader:
+                results = self.eval_step(batch)
+                for key in results.keys():
+                    if not key in Results['test_metrics'].keys():
+                        Results['test_metrics'][key] = []
+                    Results['test_metrics'][key].append(results[key])
+                progress_bar.update(1)
+            if average_metrics:
+                for key, item in Results['test_metrics'].items():
+                    Results['test_metrics'][key] = np.mean(item, axis=0)
+        if eval_disc_hist:
+            disc_hist = get_model_params_histogram(self.disc)
+            Results['disc_hist'] = disc_hist
+            progress_bar.update(1)
+        if eval_gen_hist:
+            gen_hist = get_model_params_histogram(self.gen)
+            Results['gen_hist'] = gen_hist
+            progress_bar.update(1)
+        if sample_gen_images:
+            if self.eval_batch == None:
+                assert test_dataloader != None
+                self.eval_batch = next(iter(test_dataloader))
+            sampled_images = self.sample_generated_images(self.eval_batch)
+            Result['sampled_gen_images'] = sampled_images
+            progress_bar.update(1)
+        return Results
