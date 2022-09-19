d71fe1be305a0c14dfa636ad66c48e9a686a6fb7
Jimmy Ian Gammell <jgammell@purdue.edu>
2022-09-19 15:50:21 -0400

    Added identity generator baseline

--- models/common.py
+++ models/common.py
@@ -1,7 +1,16 @@
-
+import torch
+from torch import nn
 
 def get_param_count(model):
     num_params = 0
     for _, parameter in model.named_parameters():
         num_params += parameter.numel()
     return num_params
+
+class IdentityGenerator(nn.Module):
+    def __init__(self, latent_dims, label_dims, output_shape, **kwargs):
+        super().__init__()
+        self.output_shape = output_shape
+        
+    def forward(self, latent_vars, labels):
+        return torch.zeros(latent_vars.size(0), *self.output_shape[1:], device=latent_vars.get_device())
