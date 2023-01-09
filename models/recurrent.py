158c53c2704bc32560188a37e068827822ef4a31
Jimmy Ian Gammell <jgammell@purdue.edu>
2023-01-09 15:32:01 -0500

    Wrote code for the generator and discriminator models

--- models/recurrent.py
+++ models/recurrent.py
@@ -3,35 +3,37 @@ import torch
 from torch import nn
 
 class LstmModel(nn.Module):
-    def __init__(self, layers=[64, 64, 64], delay=1, io_dims=1, realistic_hindsight=False):
+    def __init__(self, layers=[64, 64, 64], delay=1, samples_per_timestep=1, realistic_hindsight=True):
         super().__init__()
+        
+        if len(layers) > 0:
+            self.recurrent_layers = nn.ModuleList([nn.LTSMCell(samples_per_timestep, layers[0])])
+            for li, lo in zip(layers[:-1], layers[1:]):
+                self.recurrent_layers.append(nn.LSTMCell(li, lo))
+            self.recurrent_layers.append(nn.LSTMCell(layers[-1], samples_per_timestep))
+        else:
+            self.recurrent_layers = nn.ModuleList([nn.LSTMCell(samples_per_timestep, samples_per_timestep)])
         
         self.layers = layers
         self.delay = delay
-        self.io_dims = io_dims
+        self.samples_per_timestep = samples_per_timestep
         self.realistic_hindsight = realistic_hindsight
-        
-        self.recurrent_layers = nn.ModuleList([nn.LSTMCell(io_dims, layers[0])])
-        for li, lo in zip(layers[:-1], layers[1:]):
-            self.recurrent_layers.append(nn.LSTMCell(li, lo))
-        self.recurrent_layers.append(nn.Linear(layers[-1], io_dims))
-        
+    
     def forward(self, x):
         x = x.clone()
         if self.delay != 0:
             delay_padding = torch.zeros_like(x[:, :self.delay, :])
             x = torch.cat((delay_padding, x[:, :-self.delay, :]), dim=1)
-        h = [torch.zeros(x.size(0), l, device=x.device) for l in self.layers]
-        c = [torch.zeros(x.size(0), l, device=x.device) for l in self.layers]
-        mask = []
-        for x_t in x.split(1, dim=1):
+        x = x.reshape(-1, x.size(1)//self.samples_per_timestep, self.samples_per_timestep)
+        h = [torch.zeros(x.size(0), l, device=x.device) for l in self.layers+[self.samples_per_timestep]]
+        c = [torch.zeros(x.size(0), l, device=x.device) for l in self.layers+[self.samples_per_timestep]]
+        mask = torch.zeros_like(x)
+        for t, x_t in enumerate(x.split(1, dim=1)):
             x_t = x_t.squeeze(1)
-            if self.realistic_hindsight and len(mask) > 0:
-                x_t = x_t - mask[-1]
+            if self.realistic_hindsight and t > 0:
+                x_t = x_t - mask[:, t-1, :]
             h[0], c[0] = self.recurrent_layers[0](x_t, (h[0], c[0]))
-            for idx in range(len(self.recurrent_layers[1:-1])):
+            for idx in range(len(self.recurrent_layers[1:])):
                 h[idx+1], c[idx+1] = self.recurrent_layers[idx+1](h[idx], (h[idx+1], c[idx+1]))
-            o_t = self.recurrent_layers[-1](h[-1])
-            mask.append(o_t)
-        mask = torch.cat(mask, dim=1).view(x.shape)
+            mask[:, t, :] = h[-1]
         return mask
---
+++ models/resnet.py
@@ -0,0 +1,98 @@
+# This is an adaptation from Keras to PyTorch of the following file (not my own):
+#  https://github.com/google/scaaml/blob/master/scaaml/intro/model.py
+
+import numpy as np
+import torch
+from torch import nn
+
+class Block(nn.Module):
+    def __init__(self,
+                 in_channels,
+                 filters,
+                 kernel_size=3,
+                 strides=1,
+                 conv_shortcut=False,
+                 activation=nn.ReLU):
+        super().__init__()
+        
+        self.preprocess = nn.Sequential(
+            nn.BatchNorm1d(in_channels),
+            activation()
+        )
+        self.shortcut = nn.Sequential(
+            nn.Conv1d(in_channels, 4*filters, 1, stride=strides) if conv_shortcut else
+            nn.MaxPool1d(1, stride=strides) if strides > 1 else
+            nn.Identity()
+        )
+        self.conv_block = nn.Sequential(
+            nn.Conv1d(in_channels, filters, 1, bias=False, padding=1//2),
+            nn.BatchNorm1d(filters),
+            activation(),
+            nn.Conv1d(filters, filters, kernel_size, stride=strides, bias=False, padding=kernel_size//2),
+            nn.BatchNorm1d(filters),
+            activation(),
+            nn.Conv1d(filters, 4*filters, 1)
+        )
+    
+    def forward(self, x):
+        x = self.preprocess(x)
+        x = self.conv_block(x) + self.shortcut(x)
+        return x
+
+class Stack(nn.Module):
+    def __init__(self,
+                 in_channels,
+                 filters,
+                 blocks,
+                 kernel_size=3,
+                 strides=2,
+                 activation=nn.ReLU):
+        super().__init__()
+        
+        self.stack = nn.Sequential(
+            Block(in_channels, filters, kernel_size=kernel_size, activation=activation, conv_shortcut=True),
+            *[Block(filters, filters, kernel_size=kernel_size, activation=activation) for _ in range(2, blocks)],
+            Block(filters, filters, strides=strides, activation=activation)
+        )
+        
+    def forward(self, x):
+        x = self.stack(x)
+        return x
+
+class ResNet1D(nn.Module):
+    def __init__(self,
+                 input_shape,
+                 pool_size=1, # Initial pooling moved to the dataset code to avoid keeping a whole trace in ram only to downsample it
+                 filters=8,
+                 block_kernel_size=3,
+                 activation=nn.ReLU,
+                 dense_dropout=0.1,
+                 num_blocks[3, 4, 4, 3]):
+        super().__init__()
+        
+        self.preprocess = nn.Sequential(
+            nn.MaxPool1d(pool_size)
+        )
+        self.trunk = nn.Sequential(
+          *[Stack(1 if l == 0 else filters*2**l,
+                  filters*2**(l+1),
+                  num_blocks[l],
+                  kernel_size=block_kernel_size,
+                  activation=activation)
+            for l in range(4)]
+        )
+        num_features = np.prod(self.trunk(self.preprocess(torch.randn(1, *input_shape))).shape)
+        self.head = nn.Sequential(
+            nn.AvgPool1d(num_features),
+            nn.Dropout(dense_dropout),
+            nn.Linear(filters*2**4, 256),
+            nn.BatchNorm1d(256),
+            activation(),
+            nn.Linear(256, 256)
+        ) # softmax layer omitted
+        
+    def forward(self, x):
+        x = self.preprocess(x)
+        x = self.trunk(x)
+        x = self.head(x)
+        return x
